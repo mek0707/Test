de53e2116611cc919df518570bfcff48f6704c37
Mek07 <mek0987279538@hotmail.com>
2024-03-28 01:11:36 +0700

    update test

---
+++ my-app/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+export default nextConfig;
+
+// export default {
+//   basePath: "/my-app",
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/pages/index",
+//       },
+//     ];
+//   },
+// };
